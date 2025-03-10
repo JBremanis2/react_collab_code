7ce9a30fb35470703ed1b029e8ee3872626c2ddf
elglogins <elo@enterspeed.com>
2025-03-10 10:43:57 +0100

    Installed and configured tailwindcss

--- frontend/src/components/UserCard.jsx
+++ frontend/src/components/UserCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const UserCard = ({ user }) => {
     return (
-      <div className="max-w-sm bg-white shadow-lg rounded-lg p-4 border border-gray-200">
+      <div className="max-w-sm bg-white shadow-lg rounded-lg p-4 border border-gray-200 mb-6">
         <h2 className="text-lg font-semibold">{user.name}</h2>
         <p className="text-blue-600">{user.email}</p>
         <p className="text-blue-500">{user.company.name}</p>
